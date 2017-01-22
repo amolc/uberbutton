42a33e5cd61df50eb2b96c529092da40daee709c
amolc <amol.chawathe@fountaintechies.com>
2017-01-22 09:29:17 +0800

    First version commit - List Products Working

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  uber: {
+    clientId:     'ayAOzvXg9--ml2LrPpGCERrBUBCJFRkP',
+    clientSecret: 'tnxat10ZPCIauBYiHk8ZoWNvls1_726vj9NDM6fB',
+    serverToken:  'CzNytyDuYA2RxjA-PxXmIwBooicN7WH14FCi_ahP',
+    redirectUri:  'http://localhost:18000/api/callback',
+    appname: 'fountaintechies',
+    name: 'fountaintechies'
+  },
+  start_latitude:  103.8567,
+  start_longitude: 1.3115,
+  end_latitude:    1.4115,
+  end_longitude:   104.8567,
+  product_id:      '' // The product you want to order from Uber
+};
---
+++ uberbutton.js
@@ -0,0 +1,49 @@
+var request = require('request');
+var config  = require('./config');
+var express = require('express');
+var Uber = require('node-uber');
+var app = express();
+
+var TOKEN   = null;
+
+var uber = new Uber({
+  client_id: config.uber.clientId,
+  client_secret: config.uber.clientSecret ,
+  server_token: config.uber.serverToken  ,
+  redirect_uri:config.uber.redirectUri  ,
+  name: config.uber.appname,
+  language: 'en_US', // optional, defaults to en_US
+  sandbox: true // optional, defaults to false
+});
+
+app.get('/api/login', function(request, response) {
+  var url = uber.getAuthorizeUrl(['history','profile', 'request', 'places']);
+  response.redirect(url);
+});
+
+app.get('/api/callback', function(request, response) {
+   uber.authorization({
+     authorization_code: request.query.code
+   }, function(err, access_token, refresh_token) {
+     if (err) {
+       console.error(err);
+     } else {
+        console.log(access_token);
+       // store the user id and associated access token
+       // redirect the user back to your actual app
+       //response.redirect('/web/index.html');
+
+        uber.products.getAllForLocation(3.1357, 101.6880, function (err, res) {
+          if (err) console.error(err);
+          else console.log(res);
+        });
+
+     }
+   });
+});
+
+
+
+var server = app.listen(18000, function() {
+  console.log('Visit http://localhost:18000/login');
+});
